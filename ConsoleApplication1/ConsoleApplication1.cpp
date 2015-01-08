09a968969995d521701711621f07de50308fb307
hmg <tmdnldjazz@gmail.com>
2015-01-08 19:42:44 +0900

    집에가기 전 작

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -13,23 +13,23 @@
 using namespace std;
 int _tmain(int argc, _TCHAR* argv[])
 {
-	
-	regex r(R"(.*\/(.+\..+$))");
-	FILE* pf;
-	fopen_s(&pf, "a.txt", "r");
-	{
-		char str[1024] = { 0, };
-		fgets(str, 1024, pf);
-		smatch m;
-		string s = string(str);
-		if (regex_match(s,m,r))
-		{
-			cout <<m[0].str()<<endl;
-		}		
-	}
-	fclose(pf);
+	//
+	//regex r(R"(.*\/(.+\..+$))");
+	//FILE* pf;
+	//fopen_s(&pf, "a.txt", "r");
+	//{
+	//	char str[1024] = { 0, };
+	//	fgets(str, 1024, pf);
+	//	smatch m;
+	//	string s = string(str);
+	//	if (regex_match(s,m,r))
+	//	{
+	//		cout <<m[0].str()<<endl;
+	//	}		
+	//}
+	//fclose(pf);
 
-	
+
 	return 0;
 }
 
--- Dx3D/cAseLoader.cpp
+++ Dx3D/cAseLoader.cpp
@@ -116,13 +116,122 @@ void cAseLoader::Load(std::vector<LPD3DXMESH>& meshs, std::string folder, std::s
 #pragma endregion
 #pragma region ReadGeometry
 			else if (strcmp(token, ID_GEOMETRY) == 0)
-			{
+			{	
+				int nNumVertex = 0;
+				int nNumFaces = 0;
+				int nNumTVertex = 0;
+				int nNumTVFaces = 0;
+
+				std::vector<D3DXVECTOR3>		vecV;
+				std::vector<D3DXVECTOR2>		vecVT;
+				std::vector<D3DXVECTOR3>		vecVN;
+				std::vector<std::vector<int>>	vecVF;
+				std::vector<std::vector<int>>	vecTVF;
+				std::vector<ST_PNT_VERTEX>		vecVertex;
+
 				while (strcmp(token, "}") != 0)
 				{
 					fgets(read_line, 1024, pFile);
 					sscanf(read_line, "%s", token);
 					if (strcmp(token, ID_MESH) == 0)
 					{
+						while (strcmp(token, "}") != 0)
+						{
+							fgets(read_line, 1024, pFile);
+							sscanf(read_line, "%s", token);
+							if (strcmp(token, ID_MESH_NUMVERTEX) == 0)
+							{
+								sscanf(read_line, "%*s %d", &nNumVertex);
+							}
+							else if (strcmp(token, ID_MESH_NUMFACES) == 0)
+							{
+								sscanf(read_line, "%*s %d", &nNumFaces);						
+							}
+							else if (strcmp(token, ID_MESH_VERTEX_LIST) == 0)
+							{
+								float x = 0.0f, y = 0.0f, z = 0.0f;
+								for (int i = 0; i < nNumVertex; i++)
+								{
+									fgets(read_line, 1024, pFile);
+									sscanf(read_line, "%*s %*d %f %f %f", &x, &z, &y);
+									vecV.push_back(D3DXVECTOR3(x, y, z));
+								}
+							}
+							else if (strcmp(token, ID_MESH_FACE_LIST) == 0)
+							{
+								std::vector<int>	vecVertixIndex(3);
+								int a = 0, b = 0, c = 0;
+								for (int i = 0; i < nNumFaces; i++)
+								{
+									fgets(read_line, 1024, pFile);
+									sscanf(read_line, "%*s %*s %*s %d %*s %d %*s %d", &a, &c, &b);
+									vecVertixIndex[0] = a;
+									vecVertixIndex[1] = b;
+									vecVertixIndex[2] = c;
+									vecVF.push_back(vecVertixIndex);
+									vecVertixIndex.clear();									
+								}
+							}
+							else if (strcmp(token, ID_MESH_NUMTVERTEX) == 0)
+							{
+								sscanf(read_line, "%*s %d", &nNumTVertex);
+							}
+							else if (strcmp(token, ID_MESH_VERTEX_LIST) == 0)
+							{
+								float u = 0.0f, v = 0.0f;
+								for (int i = 0; i < nNumTVertex; i++)
+								{
+									fgets(read_line, 1024, pFile);
+									sscanf(read_line, "%*s %*d %f %f", &u, &v);
+									vecVT.push_back(D3DXVECTOR2(u, 1.0f - v));
+								}
+							}
+							else if (strcmp(token, ID_MESH_NUMTVFACES) == 0)
+							{
+								sscanf(read_line, "%*s %d", &nNumTVFaces);
+							}
+							else if (strcmp(token, ID_MESH_TFACELIST) == 0)
+							{
+								std::vector<int>	vecTextureIndex(3);
+								int a = 0, b = 0, c = 0;
+								for (int i = 0; i < nNumTVFaces; i++)
+								{
+									fgets(read_line, 1024, pFile);
+									sscanf(read_line, "%*s %*s %*s %d %*s %d %*s %d", &a, &c, &b);
+									vecTextureIndex[0] = a;
+									vecTextureIndex[1] = b;
+									vecTextureIndex[2] = c;
+									vecTVF.push_back(vecTextureIndex);
+									vecTextureIndex.clear();
+								}
+							}
+							else if (strcmp(token, ID_MESH_NORMALS) == 0)
+							{
+								ST_PNT_VERTEX v;
+								float x = 0.0f, y = 0.0f, z = 0.0f;
+								for (int i = 0; i < nNumFaces; i++)
+								{
+									fgets(read_line, 1024, pFile);
+									sscanf(read_line, "%s", &token);
+									if (strcmp(token, ID_MESH_FACENORMAL) == 0)
+									{
+										for (int j = 0; j < 3; j++)
+										{
+											fgets(read_line, 1024, pFile);
+											sscanf(read_line, "%*s %*d %f %f %f", &x, &z, &y);
+											v.p = vecV[vecVF[i][j]];
+											v.t = vecVT[vecTVF[i][j]];
+											v.n = D3DXVECTOR3(x, y, z);
+										}
+										vecVertex.push_back(v);
+									}
+								}
+							}
+							else
+							{
+								continue;
+							}
+						}
 
 					}
 					else if (strcmp(token, ID_MATERIAL_REF) == 0)
